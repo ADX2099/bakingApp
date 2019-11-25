cfe5a1adec0a0270c14e4cba8051590c7b2d89d9
ADX2099 <radagasth@gmail.com>
2019-11-25 16:53:17 -0600

    Initializaed App

---
+++ app/src/main/java/com/adx2099/bakingapp/ui/splash/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.adx2099.bakingapp.ui.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.adx2099.bakingapp.MainActivity;
+
+public class SplashActivity  extends AppCompatActivity implements SplashView  {
+    private final int DURATION_SPLASH = 2500;
+    private SplashPresenter splashPresenter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initApp();
+
+    }
+
+    private void initApp(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+
+            }
+        },DURATION_SPLASH);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/adx2099/bakingapp/ui/splash/SplashInteractor.java
@@ -0,0 +1,6 @@
+package com.adx2099.bakingapp.ui.splash;
+
+public class SplashInteractor implements SplashInter  {
+    private OnSplashListener listener;
+
+}
